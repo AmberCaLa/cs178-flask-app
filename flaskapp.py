35824ba34d4659e885bbb08a22f8ca843a2eba4d
AmberCaLa <ambear06@gmail.com>
2026-04-07 15:01:19 -0500

    Delete movie 2

--- flaskapp.py
+++ flaskapp.py
@@ -106,23 +106,16 @@ def add_movie():
     else:
         return render_template('add_movie.html')
 
-@app.route('/delete-user',methods=['GET', 'POST'])
+@app.route('/delete-movie',methods=['GET', 'POST'])
 def delete_user():
     if request.method == 'POST':
-        # Extract form data
         name = request.form['name']
-        
-        # Process the data (e.g., add it to a database)
-        # For now, let's just print it to the console
-        print("Name to delete:", name)
-        
 
-        flash('User deleted successfully! Hoorah!', 'warning') 
-        # Redirect to home page or another page upon successful submission
+        flash('Movie removed successfully.', 'warning') 
+
         return redirect(url_for('home'))
     else:
-        # Render the form page if the request method is GET
-        return render_template('delete_user.html')
+        return render_template('delete_movie.html')
 
 
 @app.route('/display-users')
